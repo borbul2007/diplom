f5ca9b0d620f17b9d7b78bd90d3b7e1a4ec5dc0e
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-03 15:02:14 +0300

    Update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Terraform Manifest
+# Terraform Manifest
+----
+
+Создайте сервисный аккаунт, который будет в дальнейшем использоваться Terraform для работы с инфраструктурой с необходимыми и достаточными правами. Не стоит использовать права суперпользователя
+
+Подготовьте backend для Terraform Рекомендуемый вариант: S3 bucket в созданном ЯО аккаунте(создание бакета через TF)
+
+Использовать output заполнить
+export ACCESS_KEY=""
+export SECRET_KEY=""
+terraform init -backend-config="access_key=" -backend-config=""
--- zero-step/bucket.tf
+++ zero-step/bucket.tf
@@ -1,23 +1,18 @@
-resource "yandex_kms_symmetric_key" "key" {
-  name              = "key"
+resource "yandex_kms_symmetric_key" "tfstate-bucket-key" {
+  name              = "tfstate-bucket-key"
   default_algorithm = "AES_128"
   rotation_period   = "8760h"
 }
 
 resource "yandex_storage_bucket" "tfstate" {
   bucket                = var.tfstate_bucket_name
-#  access_key            = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-#  secret_key            = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  max_size              = 1048576
-  anonymous_access_flags {
-    read        = true
-    list        = true
-    config_read = true
-  }
+  access_key            = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key            = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  max_size              = 10485760
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = yandex_kms_symmetric_key.key.id
+        kms_master_key_id = yandex_kms_symmetric_key.tfstate-bucket-key.id
         sse_algorithm     = "aws:kms"
       }
     }
