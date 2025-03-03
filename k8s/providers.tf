62eddf0c93278a018c3d558cef08a4ee840e9d48
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-03 12:04:26 +0300

    Update

--- k8s/providers.tf
+++ k8s/providers.tf
@@ -3,6 +3,19 @@ terraform {
     yandex = {
       source = "yandex-cloud/yandex"
     }
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    bucket = var.tfstate_bucket_name
+    region = "ru-central1"
+    key    = "tfstate.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true 
+    skip_s3_checksum            = true
+  }
   }
   required_version = "~>1.8.4"
 }
@@ -10,6 +23,6 @@ terraform {
 provider "yandex" {
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
-  service_account_key_file = file("~/dplm/sa-key.json")
+  service_account_key_file = file("~/dplm/key.json")
   zone                     = var.default_zone
 }
