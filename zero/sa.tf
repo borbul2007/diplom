19de7ad142b5d38d6f037730f50af11a22f055e7
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-03 11:20:59 +0300

    Update

--- zero/sa.tf
+++ zero/sa.tf
@@ -10,3 +10,4 @@ resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
 resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
   service_account_id = yandex_iam_service_account.sa.id
   description        = "Static access key for object storage"
+}
