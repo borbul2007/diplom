707b0d819a48c04db964d63c9910ead8c74e6d79
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-03 17:49:45 +0300

    Update

--- infra/k8s-master-nodes.tf
+++ infra/k8s-master-nodes.tf
@@ -16,10 +16,8 @@ resource "yandex_kubernetes_cluster" "k8s" {
     }
     security_group_ids = [yandex_vpc_security_group.k8s-sg.id]
   }
-  service_account_id      = yandex_iam_service_account.infra-editor.id
-  node_service_account_id = yandex_iam_service_account.infra-editor.id
-#  service_account_id      = yandex_iam_service_account.k8s.id
-#  node_service_account_id = yandex_iam_service_account.k8s.id
+  service_account_id      = yandex_iam_service_account.k8s.id
+  node_service_account_id = yandex_iam_service_account.k8s.id
 #  depends_on = [
 #    yandex_resourcemanager_folder_iam_member.k8s-clusters-agent,
 #    yandex_resourcemanager_folder_iam_member.vpc-public-admin,
@@ -31,10 +29,15 @@ resource "yandex_kubernetes_cluster" "k8s" {
   }
 }
 
-#resource "yandex_iam_service_account" "k8s" {
-#  name        = "k8s"
-#  description = "K8S service account"
-#}
+resource "yandex_iam_service_account" "k8s" {
+  name        = "k8s"
+  description = "K8S service account"
+}
+resource "yandex_resourcemanager_folder_iam_member" "k8s-editor" {
+  folder_id  = var.folder_id
+  role       = "editor"
+  member     = "serviceAccount:${yandex_iam_service_account.k8s.id}"
+}
 
 #resource "yandex_resourcemanager_folder_iam_member" "k8s-clusters-agent" {
 #  folder_id = var.folder_id
