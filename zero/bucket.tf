63b69239b4f50f1580bbf9819650456e6f5bf609
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-03 11:26:41 +0300

    Update

--- zero/bucket.tf
+++ zero/bucket.tf
@@ -1,5 +1,5 @@
 resource "yandex_storage_bucket" "tfstate" {
-  bucket                = var.state_bucket_name
+  bucket                = var.tfstate_bucket_name
   access_key            = yandex_iam_service_account_static_access_key.sa-static-key.access_key
   secret_key            = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
   anonymous_access_flags {
