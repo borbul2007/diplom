70738b1a03e33c1319b8c1f291464a8622ad9c17
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-03 17:38:21 +0300

    Update

--- infra/networks.tf
+++ infra/networks.tf
@@ -4,19 +4,19 @@ resource "yandex_vpc_network" "network" {
 }
 
 # VPC subnets
-resource "yandex_vpc_subnet" "public-a" {
+resource "yandex_vpc_subnet" "subnet-1" {
   name           = "subnet-1"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
-resource "yandex_vpc_subnet" "public-b" {
+resource "yandex_vpc_subnet" "subnet-2" {
   name           = "subnet-2"
   zone           = "ru-central1-b"
   network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["192.168.20.0/24"]
 }
-resource "yandex_vpc_subnet" "public-d" {
+resource "yandex_vpc_subnet" "subnet-3" {
   name           = "subnet-3"
   zone           = "ru-central1-d"
   network_id     = yandex_vpc_network.network.id
