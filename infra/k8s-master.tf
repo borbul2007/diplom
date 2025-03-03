771726f1dabc6772ddef1b06a5f73214f1098c66
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-03 17:39:20 +0300

    Update

--- infra/k8s-master.tf
+++ infra/k8s-master.tf
@@ -35,25 +35,21 @@ resource "yandex_iam_service_account" "k8s" {
 }
 
 resource "yandex_resourcemanager_folder_iam_member" "k8s-clusters-agent" {
-  folder_id = local.folder_id
   role      = "k8s.clusters.agent"
   member    = "serviceAccount:${yandex_iam_service_account.k8s.id}"
 }
 
 resource "yandex_resourcemanager_folder_iam_member" "vpc-public-admin" {
-  folder_id = local.folder_id
   role      = "vpc.publicAdmin"
   member    = "serviceAccount:${yandex_iam_service_account.k8s.id}"
 }
 
 resource "yandex_resourcemanager_folder_iam_member" "images-puller" {
-  folder_id = local.folder_id
   role      = "container-registry.images.puller"
   member    = "serviceAccount:${yandex_iam_service_account.k8s.id}"
 }
 
 resource "yandex_resourcemanager_folder_iam_member" "encrypterDecrypter" {
-  folder_id = local.folder_id
   role      = "kms.keys.encrypterDecrypter"
   member    = "serviceAccount:${yandex_iam_service_account.k8s.id}"
 }
