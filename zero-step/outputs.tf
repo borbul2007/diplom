d8e9bf056dfabbcf046c66d51dcdc2df9eb52df4
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-04 09:21:26 +0300

    Update

--- zero-step/outputs.tf
+++ zero-step/outputs.tf
@@ -1,13 +1,14 @@
 output "tfstate-bucket_access-key" {
-  value = nonsensitive(yandex_iam_service_account_static_access_key.sa-static-key.access_key)
+  value = yandex_iam_service_account_static_access_key.sa-static-key.access_key
 }
 output "tfstate-bucket_secret-key" {
-  value = nonsensitive(yandex_iam_service_account_static_access_key.sa-static-key.secret_key)
+  sensitive   = true
+  value       = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
 }
 
-output "access_key" {
+output "tfstate-bucket_access_key" {
   value = data.yandex_lockbox_secret_version.tfstate-bucket_version.entries[1].text_value
 }
-output "secret_key" {
+output "tfstate-bucket_secret_key" {
   value = data.yandex_lockbox_secret_version.tfstate-bucket_version.entries[0].text_value
 }
