9f6f63b4eca3fdb31be59101e849dda938b81731
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-04 09:40:41 +0300

    Update

--- zero-step/bucket.tf
+++ zero-step/bucket.tf
@@ -5,8 +5,10 @@ resource "yandex_kms_symmetric_key" "tfstate-bucket-key" {
 }
 
 resource "yandex_storage_bucket" "tfstate" {
-  bucket                = var.tfstate_bucket_name
-  max_size              = 10485760
+#  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+#  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket     = var.tfstate_bucket_name
+  max_size   = 10485760
   anonymous_access_flags {
     read        = false
     list        = false
--- zero-step/outputs.tf
+++ zero-step/outputs.tf
@@ -1,13 +1,13 @@
-output "tfstate-bucket_access-key" {
-  value = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-}
-output "tfstate-bucket_secret-key" {
-  value = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-}
+#output "tfstate-bucket_access-key_" {
+#  value = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+#}
+#output "tfstate-bucket_secret-key_" {
+#  value = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+#}
 
-output "tfstate-bucket_access_key_" {
+output "tfstate-bucket_access_key" {
   value = data.yandex_lockbox_secret_version.tfstate-bucket_version.entries[1].text_value
 }
-output "tfstate-bucket_secret_key_" {
+output "tfstate-bucket_secret_key" {
   value = data.yandex_lockbox_secret_version.tfstate-bucket_version.entries[0].text_value
 }
