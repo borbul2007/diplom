76ddcf137d969f808ff07054cfa24880e153b54a
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-05 15:09:27 +0300

    Update

--- infra/k8s-nodes.tf
+++ infra/k8s-nodes.tf
@@ -6,12 +6,12 @@ resource "yandex_iam_service_account" "k8s" {
   name        = "k8s"
   description = "Service account for managing K8S instance group"
 }
-#resource "yandex_resourcemanager_folder_iam_member" "k8s-editor" {
-#  folder_id  = var.folder_id
-#  role       = "editor"
-#  member    = "serviceAccount:${yandex_iam_service_account.k8s.id}"
-#  depends_on = [yandex_iam_service_account.k8s]
-#}
+resource "yandex_resourcemanager_folder_iam_member" "k8s-editor" {
+  folder_id  = var.folder_id
+  role       = "editor"
+  member    = "serviceAccount:${yandex_iam_service_account.k8s.id}"
+  depends_on = [yandex_iam_service_account.k8s]
+}
 
 resource "yandex_compute_instance_group" "k8s-nodes" {
   name                = "k8s-nodes"
