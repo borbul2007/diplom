4a5d71cda36a39310cdb98be69c648efae27b279
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-05 14:33:44 +0300

    Update

--- infra/k8s-nodes.tf
+++ infra/k8s-nodes.tf
@@ -2,22 +2,22 @@ data "yandex_compute_image" "vm-image" {
   family = var.image_family
 }
 
-resource "yandex_iam_service_account" "k8s" {
-  name        = "k8s"
-  description = "Service account for managing K8S instance group"
-}
-resource "yandex_resourcemanager_folder_iam_member" "k8s-editor" {
-  folder_id  = var.folder_id
-  role       = "editor"
-  member    = "serviceAccount:${yandex_iam_service_account.k8s.id}"
-  depends_on = [yandex_iam_service_account.k8s]
-}
+#resource "yandex_iam_service_account" "k8s" {
+#  name        = "k8s"
+#  description = "Service account for managing K8S instance group"
+#}
+#resource "yandex_resourcemanager_folder_iam_member" "k8s-editor" {
+#  folder_id  = var.folder_id
+#  role       = "editor"
+#  member    = "serviceAccount:${yandex_iam_service_account.k8s.id}"
+#  depends_on = [yandex_iam_service_account.k8s]
+#}
 
 resource "yandex_compute_instance_group" "k8s-nodes" {
   name                = "k8s-nodes"
   folder_id           = var.folder_id
   service_account_id  = "${yandex_iam_service_account.k8s.id}"
-  depends_on          = [yandex_resourcemanager_folder_iam_member.k8s-editor]
+#  depends_on          = [yandex_resourcemanager_folder_iam_member.k8s-editor]
   instance_template {
     name = "k8s-node-{instance.index}"
     platform_id = var.instance_platform_id
