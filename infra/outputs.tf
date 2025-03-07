e46b78728fe3c8b5b897e08d5cce97bd0171ad5b
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-07 10:52:18 +0300

    Update

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,21 +1,21 @@
 output "instance_group_k8s_master_nodes_public_ips" {
-  description = "Public IP addresses for K8S master-nodes"
+  description = "Public IP addresses for K8S master nodes"
   value = yandex_compute_instance_group.k8s-master-nodes.instances.*.network_interface.0.nat_ip_address
 }
 
 output "instance_group_k8s_masters_nodes_private_ips" {
-  description = "Private IP addresses for K8S master-nodes"
-  value = yandex_compute_instance_group.k8s-masters.instances.*.network_interface.0.ip_address
+  description = "Private IP addresses for K8S master nodes"
+  value = yandex_compute_instance_group.k8s-masters-nodes.instances.*.network_interface.0.ip_address
 }
 
 output "instance_group_k8s_workers_nodes_public_ips" {
-  description = "Public IP addresses for K8S worder-nodes"
-  value = yandex_compute_instance_group.k8s-workers.instances.*.network_interface.0.nat_ip_address
+  description = "Public IP addresses for K8S worker nodes"
+  value = yandex_compute_instance_group.k8s-workers-nodes.instances.*.network_interface.0.nat_ip_address
 }
 
 output "instance_group_k8s_workers_nodes_private_ips" {
-  description = "Private IP addresses for K8S worker-nodes"
-  value = yandex_compute_instance_group.k8s-workers.instances.*.network_interface.0.ip_address
+  description = "Private IP addresses for K8S worker nodes"
+  value = yandex_compute_instance_group.k8s-workers-nodes.instances.*.network_interface.0.ip_address
 }
 
 output "network_load_balancer_public_ip" {
