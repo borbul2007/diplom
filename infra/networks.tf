b934c064d63252f57ea6c5cd265a18bd030534c5
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-07 09:06:10 +0300

    Update

--- infra/networks.tf
+++ infra/networks.tf
@@ -4,11 +4,11 @@ resource "yandex_vpc_network" "k8s" {
 
 resource "yandex_vpc_subnet" "subnet" {
   count          = length(var.k8s_networks)
-  network_id     = yandex_vpc_network.network.id
+  network_id     = yandex_vpc_network.k8s.id
   name           = "subnet-${count.index}"
   zone           = var.k8s_networks[count.index].zone
   v4_cidr_blocks = [var.k8s_networks[count.index].cidr]
-  depends_on     = [yandex_vpc_network.network]
+  depends_on     = [yandex_vpc_network.k8s]
 }
 
 /*
