bdc39cace9d0bc5ce69e4cecdd0c12993b589fe7
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-07 12:39:39 +0300

    Update

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,13 +1,13 @@
-output "instance_group_k8s_master_nodes_public_ips" {
+output "k8s_master_nodes_public_ips" {
   value = yandex_compute_instance_group.k8s-master-nodes.instances.*.network_interface.0.nat_ip_address
 }
-output "instance_group_k8s_masters_nodes_private_ips" {
+output "k8s_masters_nodes_private_ips" {
   value = yandex_compute_instance_group.k8s-master-nodes.instances.*.network_interface.0.ip_address
 }
-output "instance_group_k8s_workers_nodes_public_ips" {
+output "k8s_workers_nodes_public_ips" {
   value = yandex_compute_instance_group.k8s-worker-nodes.instances.*.network_interface.0.nat_ip_address
 }
-output "instance_group_k8s_workers_nodes_private_ips" {
+output "k8s_workers_nodes_private_ips" {
   value = yandex_compute_instance_group.k8s-worker-nodes.instances.*.network_interface.0.ip_address
 }
 output "network_load_balancer_public_ip" {
