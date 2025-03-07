1b2dbf6f685c2b24a581caa34324630e981ed18f
root <root@nt.ksob.lan>
2025-03-07 16:13:28 +0300

    Update

---
+++ zero-step/do-key.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export ACCESS_KEY=$(terraform output tfstate-bucket_access_key)
+export SECRET_KEY=$(terraform output tfstate-bucket_secret_key)
+yc iam key create --service-account-name infra --output key.json && mv key.json ~/dplm
+
+
