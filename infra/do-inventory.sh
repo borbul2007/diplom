0c799310cf5fc1aa4c1f682d19830e867adeeef3
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-07 14:32:17 +0300

    Update

--- infra/do-inventory.sh
+++ infra/do-inventory.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-echo "[kube_control_plane]" >> inventory.ini
-echo "\n" >> inventory.ini
+echo "[kube_control_plane]" > inventory.ini
+echo "" >> inventory.ini
 for i in 1 2 3; do
-echo "node${i} ansible_host=$(terraform output -json k8s_master_nodes_public_ips | jq -j ".[${i}]") ip=$(terraform output -json k8s_master_nodes_private_ips | jq -j ".[${i}]") etcd_member_name=etcd1"  >> inventory.ini
-echo "\n" >> inventory.ini
+  echo "node${i} ansible_host=(terraform output -json k8s_master_nodes_public_ips | jq -j ".[${i}]") ip=(terraform output -json k8s_master_nodes_private_ips | jq -j ".[${i}]") etcd_member_name=etcd1"  >> inventory.ini
+done
+echo "" >> inventory.ini
 echo "[etcd:children]" >> inventory.ini
 echo "kube_control_plane" >> inventory.ini
-echo "\n" >> inventory.ini
-[kube_node]
-for i in 1 2 ; do
-echo "node4 ansible_host=$(terraform output -json k8s_workers_nodes_public_ips | jq -j ".[${i}]") ip=$(terraform output -json k8s_workers_nodes_private_ips | jq -j ".[${i}]") >> inventory.ini
+echo "" >> inventory.ini
+echo "[kube_node]" >> inventory.ini
+for i in 4 5 ; do
+  echo "node${i} ansible_host=(terraform output -json k8s_workers_nodes_public_ips | jq -j ".[${i}]") ip=(terraform output -json k8s_workers_nodes_private_ips | jq -j ".[${i}]")" >> inventory.ini
+done
