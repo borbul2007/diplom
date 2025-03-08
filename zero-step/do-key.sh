9b161fd88e1b7eac634154f0d2b392ec5ef2f181
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-08 11:31:13 +0300

    Update

--- zero-step/do-key.sh
+++ zero-step/do-key.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#export ACCESS_KEY=$(terraform output tfstate-bucket_access_key)
-#export SECRET_KEY=$(terraform output tfstate-bucket_secret_key)
-export ACCESS_KEY=$(terraform output access_key)
-export SECRET_KEY=$(terraform output secret_key)
+export ACCESS_KEY=$(echo "access_key=$(terraform output tfstate-bucket_access_key)" | sed -e "s/_key=\"/_key=/g")
+export SECRET_KEY=$(echo "secret_key=$(terraform output tfstate-bucket_secret_key)" | sed -e "s/_key=\"/_key=/g")
 yc iam key create --service-account-name infra --output key.json && mv key.json ~/dplm
--- zero-step/outputs.tf
+++ zero-step/outputs.tf
@@ -1,9 +1,7 @@
-#output "tfstate-bucket_access_key" {
-output "access_key" {  
+output "tfstate-bucket_access_key" {
   value = data.yandex_lockbox_secret_version.tfstate-bucket_version.entries[1].text_value
 }
-#output "tfstate-bucket_secret_key" {
-output "secret_key" {
+output "tfstate-bucket_secret_key" {
   value = data.yandex_lockbox_secret_version.tfstate-bucket_version.entries[0].text_value
 }
 
