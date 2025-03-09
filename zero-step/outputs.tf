7f2a1ef6287a241dd719a337aa0bcd7a7dfab7fc
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-09 10:56:32 +0300

    Update

--- zero-step/outputs.tf
+++ zero-step/outputs.tf
@@ -5,5 +5,5 @@ output "tfstate-bucket_secret_key" {
   value = data.yandex_lockbox_secret_version.tfstate-bucket_version.entries[0].text_value
 }
 output "jump-host_public_ip" {
-  value = yandex_compute_instance.jump-host.instances.*.network_interface.0.nat_ip_address
+  value = yandex_compute_instance.jump-host.instance.*.network_interface.0.nat_ip_address
 }
