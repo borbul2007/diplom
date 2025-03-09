418c612fd62eab8b22cdb31a706fb0b5a926714f
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-09 10:50:39 +0300

    Update

--- zero-step/networks.tf
+++ zero-step/networks.tf
@@ -1,35 +1,33 @@
 resource "yandex_vpc_network" "k8s" {
   name       = "k8s"
 }
-
+/*
 resource "yandex_vpc_subnet" "k8s" {
   count          = length(var.k8s_networks)
   network_id     = yandex_vpc_network.k8s.id
   name           = "k8s-${count.index}"
   zone           = var.k8s_networks[count.index].zone
   v4_cidr_blocks = [var.k8s_networks[count.index].cidr]
-  depends_on     = [yandex_vpc_network.k8s]
 }
+*/
 
-/*
-resource "yandex_vpc_subnet" "k8s-1" {
-  name           = "k8s-1"
+resource "yandex_vpc_subnet" "k8s-0" {
+  name           = "k8s-0"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.k8s.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
-resource "yandex_vpc_subnet" "k8s-2" {
-  name           = "k8s-2"
+resource "yandex_vpc_subnet" "k8s-1" {
+  name           = "k8s-1"
   zone           = "ru-central1-b"
   network_id     = yandex_vpc_network.k8s.id
   v4_cidr_blocks = ["192.168.20.0/24"]
 }
 
-resource "yandex_vpc_subnet" "k8s-3" {
-  name           = "k8s-3"
+resource "yandex_vpc_subnet" "k8s-2" {
+  name           = "k8s-2"
   zone           = "ru-central1-d"
   network_id     = yandex_vpc_network.k8s.id
   v4_cidr_blocks = ["192.168.30.0/24"]
 }
-*/
