810a1c905788c4cfe4c28ceb574bac10e6a10a11
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-10 19:48:46 +0300

    Update

--- zero-step/do-key.sh
+++ zero-step/do-key.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ACCESS_KEY=$(echo "\"access_key=$(terraform output tfstate-bucket_access_key)" | sed -e "s/_key=\"/_key=/g")
-export SECRET_KEY=$(echo "\"secret_key=$(terraform output tfstate-bucket_secret_key)" | sed -e "s/_key=\"/_key=/g") 
-export PUBLIC_IP=$(echo"yandex_compute_instance.jump-host.network_interface.0.nat_ip_address")
-yc iam key create --service-account-name infra --output key.json && mv key.json ~/dplm
+echo "export ACCESS_KEY=$(echo "\"access_key=$(terraform output tfstate-bucket_access_key)" | sed -e "s/_key=\"/_key=/g")" >> .profile
+echo "export SECRET_KEY=$(echo "\"secret_key=$(terraform output tfstate-bucket_secret_key)" | sed -e "s/_key=\"/_key=/g")" >> .profile
+echo "export PUBLIC_IP=$(terraform output jump-host_public_ip)" >> .profile
+yc iam key create --service-account-name infra --output infra.json
