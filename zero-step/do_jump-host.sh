99fabb638c64205b566c64bf7436b8c7a72b1249
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-11 10:31:23 +0300

    Update

--- zero-step/do_jump-host.sh
+++ zero-step/do_jump-host.sh
@@ -8,7 +8,7 @@ echo "export TF_VAR_yandex_vpc_subnet_k8s-0_id=$(terraform output yandex_vpc_sub
 echo "export TF_VAR_yandex_vpc_subnet_k8s-1_id=$(terraform output yandex_vpc_subnet_k8s-1_id)" >> resources/.profile
 echo "export TF_VAR_yandex_vpc_subnet_k8s-2_id=$(terraform output yandex_vpc_subnet_k8s-2_id)" >> resources/.profile
 
-#yc iam key create --service-account-name infra --output resources/infra.json
+yc iam key create --service-account-name infra --output resources/infra.json
 
 scp -i ~/nt-ssh/id_ed25519 ~/nt-ssh/* ubuntu@$(terraform output jump-host_public_ip | xargs):/home/ubuntu/keys/
 scp -i ~/nt-ssh/id_ed25519 ./resources/infra.json ubuntu@$(terraform output jump-host_public_ip | xargs):/home/ubuntu/keys/
