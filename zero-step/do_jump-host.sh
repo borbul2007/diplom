bf1c43605d041d1a933826bf21e97fd0e164cb33
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-11 10:14:13 +0300

    Update

--- zero-step/do_jump-host.sh
+++ zero-step/do_jump-host.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "export ACCESS_KEY=$(echo "\"access_key=$(terraform output tfstate-bucket_access_key)" | sed -e "s/_key=\"/_key=/g")" >> resources/.profile
+echo "export ACCESS_KEY=$(echo "\"access_key=$(terraform output tfstate-bucket_access_key)" | sed -e "s/_key=\"/_key=/g")" > resources/.profile
 echo "export SECRET_KEY=$(echo "\"secret_key=$(terraform output tfstate-bucket_secret_key)" | sed -e "s/_key=\"/_key=/g")" >> resources/.profile
 echo "export PUBLIC_IP=$(terraform output jump-host_public_ip)" >> resources/.profile
 echo "export TF_VAR_yandex_vpc_network_k8s_id=$(terraform output yandex_vpc_network_k8s_id)" >> resources/.profile
