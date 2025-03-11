abb44363be77a3fafe677a04643847e53776d544
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-11 17:09:04 +0300

    Update

--- README.md
+++ README.md
@@ -7,11 +7,11 @@ yc iam key create --service-account-name nt-terraform --output key.json && mv ~k
 git clone https://github.com/borbul2007/diplom.git
 cd ~/diplom/zero-step
 terraform init && terraform apply -auto-approve
-chmod +x do_jump-host.sh && ./do_jump-host.sh
+./do_jump-host.sh
 
 ## On jump host
-terraform version && kubectl version && helm version && yc version
+terraform version && kubectl version && sudo helm version && yc version
 cd ~/diplom/infra
 #cp .terraformrc ~
 terraform init -backend-config=${ACCESS_KEY} -backend-config=${SECRET_KEY} && terraform apply -auto-approve
-chmod +x do_k8s-cluster.sh && ./do_k8s-cluster.sh
+./do_k8s-cluster.sh
--- infra/do-inventory.sh
+++ infra/do-inventory.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-#echo -e "[kube_control_plane]\n" > inventory.ini
-#for i in 0 1 2 ; do
-#  echo "node$((${i}+1)) ansible_host=$(terraform output -json k8s_master_nodes_public_ips | jq -j ".[${i}]") ip=$(terraform output -json k8s_master_nodes_private_ips | jq -j ".[${i}]") etcd_member_name=etcd1"  >> inventory.ini
-#done
-#echo -e "\n[etcd:children]\nkube_control_plane\n\n[kube_node]" >> inventory.ini
-#for i in 0 1 ; do
-#  echo "node$((${i}+4)) ansible_host=$(terraform output -json k8s_worker_nodes_public_ips | jq -j ".[${i}]") ip=$(terraform output -json k8s_worker_nodes_private_ips | jq -j ".[${i}]")" >> inventory.ini
-#done
+#python3 -m venv venv && source ./venv/bin/activate && pip3 install -r requirements.txt
+#cp -rfp ./inventory/sample ./inventory/k8s
+echo -e "[kube_control_plane]\n" > ./inventory/k8s/inventory.ini
+for i in 0 1 2 ; do
+  echo "node$((${i}+1)) ip=$(terraform output -json k8s_master_nodes_private_ips | jq -j ".[${i}]") etcd_member_name=etcd1"  >> inventory.ini
+done
+echo -e "\n[etcd:children]\nkube_control_plane\n\n[kube_node]" >> inventory.ini
+for i in 0 1 ; do
+  echo "node$((${i}+4)) ip=$(terraform output -json k8s_worker_nodes_private_ips | jq -j ".[${i}]")" >> inventory.ini
+done
 
-cd ~/kubespray
-sudo pip3 install -r requirements.txt
-ansible-playbook -i inventory.ini --private-key ~/keys/id_ed25519 cluster.yml
+#cd ~/kubespray
+#ansible-playbook -i ./inventory/k8s/ -u $USERNAME -b -v --private-key ~/keys/id_ed25519 cluster.yml
