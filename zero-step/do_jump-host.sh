6686ca2fb0aad490dcabcafdb2e60a52ae24c2ca
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-11 10:37:00 +0300

    Update

--- zero-step/do_jump-host.sh
+++ zero-step/do_jump-host.sh
@@ -3,12 +3,12 @@
 echo "export ACCESS_KEY=$(echo "\"access_key=$(terraform output tfstate-bucket_access_key)" | sed -e "s/_key=\"/_key=/g")" > resources/.profile
 echo "export SECRET_KEY=$(echo "\"secret_key=$(terraform output tfstate-bucket_secret_key)" | sed -e "s/_key=\"/_key=/g")" >> resources/.profile
 echo "export PUBLIC_IP=$(terraform output jump-host_public_ip)" >> resources/.profile
-echo "export TF_VAR_yandex_vpc_network_k8s_id=$(terraform output yandex_vpc_network_k8s_id)" >> resources/.profile
-echo "export TF_VAR_yandex_vpc_subnet_k8s-0_id=$(terraform output yandex_vpc_subnet_k8s-0_id)" >> resources/.profile
-echo "export TF_VAR_yandex_vpc_subnet_k8s-1_id=$(terraform output yandex_vpc_subnet_k8s-1_id)" >> resources/.profile
-echo "export TF_VAR_yandex_vpc_subnet_k8s-2_id=$(terraform output yandex_vpc_subnet_k8s-2_id)" >> resources/.profile
+echo "export TF_VAR_network_k8s_id=$(terraform output yandex_vpc_network_k8s_id)" >> resources/.profile
+echo "export TF_VAR_subnet_k8s-0_id=$(terraform output yandex_vpc_subnet_k8s-0_id)" >> resources/.profile
+echo "export TF_VAR_subnet_k8s-1_id=$(terraform output yandex_vpc_subnet_k8s-1_id)" >> resources/.profile
+echo "export TF_VAR_subnet_k8s-2_id=$(terraform output yandex_vpc_subnet_k8s-2_id)" >> resources/.profile
 
-yc iam key create --service-account-name infra --output resources/infra.json
+#yc iam key create --service-account-name infra --output resources/infra.json
 
 scp -i ~/nt-ssh/id_ed25519 ~/nt-ssh/* ubuntu@$(terraform output jump-host_public_ip | xargs):/home/ubuntu/keys/
 scp -i ~/nt-ssh/id_ed25519 ./resources/infra.json ubuntu@$(terraform output jump-host_public_ip | xargs):/home/ubuntu/keys/
