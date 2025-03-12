a028d70f2b056a0a42d3c6c7f3c6a3eedd143324
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-12 11:25:03 +0300

    Update

--- infra/do_k8s-cluster.sh
+++ infra/do_k8s-cluster.sh
@@ -4,7 +4,7 @@ cd ~/diplom/infra
 echo -e "[kube_control_plane]\n" > ./resources/inventory.ini
 for i in 0 1 2 ; do echo "k8s-master-node-$((${i}+1)) ip=$(terraform output -json k8s_master_nodes_private_ips | jq -j ".[${i}]") etcd_member_name=etcd1"  >> ./resources/inventory.ini; done
 echo -e "\n[etcd:children]\nkube_control_plane\n\n[kube_node]" >> ./resources/inventory.ini
-for i in 0 1 ; do echo "k8s-worker-node-$((${i}+4)) ip=$(terraform output -json k8s_worker_nodes_private_ips | jq -j ".[${i}]")" >> ./resources/inventory.ini; done
+for i in 0 1 ; do echo "k8s-worker-node-$((${i}+1)) ip=$(terraform output -json k8s_worker_nodes_private_ips | jq -j ".[${i}]")" >> ./resources/inventory.ini; done
 
 cd ~
 python3 -m venv venv && source ./venv/bin/activate
