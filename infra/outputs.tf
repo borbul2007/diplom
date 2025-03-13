093ceacf9e5b6eaaf1614c3726aa1b4851c3e781
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-03-13 10:21:18 +0300

    Update

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -7,6 +7,3 @@ output "k8s_worker_nodes_private_ips" {
 output "network_load_balancer_public_ip" {
   value = yandex_lb_network_load_balancer.k8s.listener.*.external_address_spec[0].*.address
 }
-output "k8s_api_node_private_ip" {
-  value = yandex_compute_instance_group.k8s-master-nodes.instances.0.network_interface.0.ip_address
-}
